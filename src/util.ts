da8bc82bc172ebf2245d1dc763055a9e47a69566
qysp <qysp.yo@gmail.com>
2021-03-12 09:50:19 +0100

    enhance typing in utils

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,18 @@
-const UidGenerator = (function* (id = 0, suffix = Math.random()) {
-  while (++id) yield (id + suffix).toString(36);
+const UidGenerator = (function* (id: number = 0): Generator<string, void> {
+  while (++id) yield (id + Math.random()).toString(36);
 })();
 
 export const uid = (): string => UidGenerator.next().value as string;
 
-export const createFragment = (html: string) => {
+export const createFragment = (html: string): DocumentFragment => {
   const template = document.createElement('template');
   template.innerHTML = html;
   return template.content;
 };
 
-export const has = <O extends {}, P extends string | number>(obj: O, prop: P): obj is O & Record<P, unknown> => {
+export const has = <O extends {}, P extends string | number>(
+  obj: O,
+  prop: P,
+): obj is O & Record<P, O[keyof O]> => {
   return Object.prototype.hasOwnProperty.call(obj, prop);
 };
